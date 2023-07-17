bc7deccd342d0e43d551b82a4dab01c06bd0fb7b
Marcos Junior <mod.gluguer@gmail.com>
2023-07-17 00:19:35 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   )
